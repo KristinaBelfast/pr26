92278d5709252fb8668182e18f981abb9a20b75c
kristinabelfast <vnukova.kristina.study@bk.ru>
2024-12-19 19:16:16 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pr26/ui/theme/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pr26.ui.theme
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.pr26.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
